07488e8ecf85607de2911fe07a09cc99d9f8d5af
Boris Rybalkin <ribalkin@gmail.com>
2023-12-06 14:17:44 +0000

    fix log clean

--- bin/service.nginx.sh
+++ bin/service.nginx.sh
@@ -3,6 +3,6 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
 
-/bin/rm ${SNAP_COMMON}/log/nginx_error.log
+/bin/rm -f ${SNAP_COMMON}/log/nginx_error.log
 /bin/rm -f ${SNAP_COMMON}/web.socket
 exec ${DIR}/nginx/sbin/nginx -c ${SNAP_DATA}/config/nginx.conf -p ${DIR}/nginx -e stderr
