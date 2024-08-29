f410846b0aca54683c209e054081d0e38b7db4e4
Boris Rybalkin <ribalkin@gmail.com>
2024-08-29 15:49:04 +0100

    nginx fix

--- bin/service.nginx.sh
+++ bin/service.nginx.sh
@@ -5,4 +5,4 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
 /bin/rm -f ${SNAP_COMMON}/log/nginx_error.log
 /bin/rm -f ${SNAP_COMMON}/web.socket
-exec ${DIR}/nginx/sbin/nginx -c ${SNAP_DATA}/config/nginx.conf -p ${DIR}/nginx -e stderr
+exec ${DIR}/nginx/bin/nginx.sh -c ${SNAP_DATA}/config/nginx.conf -p ${DIR}/nginx -e stderr
